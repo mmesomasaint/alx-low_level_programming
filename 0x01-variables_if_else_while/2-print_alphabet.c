4495d0c068344fc5c04a70fa0ac57462efc414dc
mmesomasaint <mmesomasaint@gmail.com>
2022-10-14 21:04:32 -0700

    Prints all in lowercase and new line source added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}

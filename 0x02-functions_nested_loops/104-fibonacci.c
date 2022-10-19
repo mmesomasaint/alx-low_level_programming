5e7384bc79ea6a1d5c1cbf51b185aa92d72f87f3
mmesomasaint <mmesomasaint@gmail.com>
2022-10-18 22:39:58 -0700

    Updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,7 +32,7 @@ while (i <= 98)
 {
 if (f1o > 0)
 printf("%lu", f1o);
-iinitial0s = numLength(mx) - 1 - numLength(f1);
+initial0s = numLength(mx) - 1 - numLength(f1);
 while (f1o > 0 && initial0s > 0)
 {
 printf("%i", 0);

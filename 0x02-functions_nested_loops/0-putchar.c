444c954ef10f2135c913943f7b08cda63f9ecd46
mmesomasaint <mmesomasaint@gmail.com>
2022-10-18 20:05:24 -0700

    Print putchar to screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints 'putchar \n'
+ * Return: 0
+ */
+int main(void)
+{
+char str[] = "putchar\n";
+int i = 0;
+while (str[i] != '\0')
+{
+char c = str[i];
+_putchar(c);
+i++;
+}
+return (0);
+}

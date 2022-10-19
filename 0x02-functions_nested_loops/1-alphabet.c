37c8542e9b24c9e1058349f86f97938402b45c69
mmesomasaint <mmesomasaint@gmail.com>
2022-10-18 20:11:18 -0700

    Function that prints alphabet added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function taht prints the alphabet,
+ * in lowercase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}

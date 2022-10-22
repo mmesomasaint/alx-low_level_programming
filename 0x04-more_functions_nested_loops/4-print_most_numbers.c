894685d03593c252d3f95bb71431975a550a7946
mmesomasaint <mmesomasaint@gmail.com>
2022-10-21 20:13:57 -0700

    Prints most numbers except 2 numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#includde "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * description: prints numbers from 0 - 9 except 2 and 3
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int a = 0;
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
+}
+_putchar('\n');
+}

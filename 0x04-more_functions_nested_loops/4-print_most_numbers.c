134e64cafbe4b38893b2d62d915480885c8d5be0
mmesomasaint <mmesomasaint@gmail.com>
2022-10-21 20:18:08 -0700

    Updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #includde "main.h"
 
 /**
- * print_most_numbers - prints 0 - 9 except 2, 3
- * description: prints numbers from 0 - 9 except 2 and 3
+ * print_most_numbers - prints 0 - 9 except 2, 4
+ * description: prints numbers from 0 - 9 except 2 and 4
  * Return: void
  */
 
@@ -13,7 +13,7 @@ while (a <= 9)
 {
 if (a != 2 && a != 4)
 {
-_putchar(a + '0');
+_putchar(a);
 }
 a++;
 }

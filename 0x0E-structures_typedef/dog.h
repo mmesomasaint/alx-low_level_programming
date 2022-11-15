c210b985284c43e19f3035d62b23bbbe17b2ce60
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2022-11-15 04:07:21 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,13 @@ typedef struct dog dog_t;
  * @owner: third member
  */
 struct dog {
-char * name,
-float age,
-char * owner
+char * name;
+float age;
+char * owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name
 
 #endif
